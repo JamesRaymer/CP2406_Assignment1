97ffc3e7e4c981c6fe43ec93e71f39fc272f8a07
jamesraymer <james.raymer@my.jcu.edu.au>
2019-09-04 05:36:58 +1000

    Add constructor to road class and helper methods.

--- src/TrafficSimulator/Road.java
+++ src/TrafficSimulator/Road.java
@@ -1,5 +1,23 @@
 package TrafficSimulator;
 
 class Road {
+    private int segments;
+    private String type;
 
+    Road(int segments, String type) {
+        this.segments = segments;
+        this.type = type;
+    }
+
+    int getSegments() {
+        return this.segments;
+    }
+
+    void setType(String type) {
+        this.type = type;
+    }
+
+    String getType() {
+        return this.type;
+    }
 }
