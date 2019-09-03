f9c7751779137fc98f984c90dd11ba2a41a6ed4b
jamesraymer <james.raymer@my.jcu.edu.au>
2019-09-04 05:53:40 +1000

    Add travel conditions and TrafficLight methods to Simulator class.

--- src/TrafficSimulator/Simulator.java
+++ src/TrafficSimulator/Simulator.java
@@ -10,5 +10,26 @@ public class Simulator {
         int firstEndPoint = road1.getSegments();
         int secondEndPoint = road2.getSegments();
         byte roadSpeed = 50;
+
+        while(firstEndPoint != 0) {
+            --firstEndPoint;
+            System.out.println("The " + vehicle.getType() + " is traveling at a speed of " + roadSpeed + "km/hr + \nand is " + 25 * firstEndPoint + " meters away from the end of the road.");
+        }
+
+        trafficLight.changeStatus();
+
+        for(boolean condition = trafficLight.getStatus(); !condition; condition = trafficLight.getStatus()) {
+            trafficLight.redLight();
+            trafficLight.changeStatus();
+        }
+
+        trafficLight.greenLight();
+
+        while(secondEndPoint != 0) {
+            --secondEndPoint;
+            System.out.println("The " + vehicle.getType() + " is traveling at a speed of " + roadSpeed + "km/hr + \nand is " + 25 * secondEndPoint + " meters away from the end of the road.");
+        }
+
+        System.out.println("Car has fell off the world and has ceased to exist.");
     }
 }
