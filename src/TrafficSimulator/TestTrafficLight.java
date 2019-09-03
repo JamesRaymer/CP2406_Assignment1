c40ab11c84e0ffaa4950f2f3c88a3ce721319b47
jamesraymer <james.raymer@my.jcu.edu.au>
2019-09-04 05:50:56 +1000

    Add JUnit testing for TrafficLight class.

--- src/TrafficSimulator/TestTrafficLight.java
+++ src/TrafficSimulator/TestTrafficLight.java
@@ -1,6 +1,16 @@
 package TrafficSimulator;
-import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class TestTrafficLight {
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TestTrafficLight {
+    public TestTrafficLight() {
+    }
+
+    @Test
+    public void checkTrafficLight() {
+        TrafficLight trafficLight = new TrafficLight(true);
+        Boolean condition = true;
+        Assertions.assertEquals(trafficLight.getStatus(), condition);
+    }
 }
