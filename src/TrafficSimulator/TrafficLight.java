fd9f9866b62576c3b616b883bda715d519e82741
jamesraymer <james.raymer@my.jcu.edu.au>
2019-09-04 05:50:13 +1000

    Add TrafficLight constructor and helper methods.

--- src/TrafficSimulator/TrafficLight.java
+++ src/TrafficSimulator/TrafficLight.java
@@ -1,5 +1,28 @@
 package TrafficSimulator;
-import java.util.Random;
-class TrafficLight {
 
+import java.util.Random;
+
+class TrafficLight {
+    private Boolean condition;
+
+    TrafficLight(Boolean condition) {
+        this.condition = condition;
+    }
+
+    Boolean getStatus() {
+        return this.condition;
+    }
+
+    void changeStatus() {
+        Random random = new Random();
+        this.condition = random.nextBoolean();
+    }
+
+    void greenLight() {
+        System.out.println("The traffic light is green. The car is moving!");
+    }
+
+    void redLight() {
+        System.out.println("The traffic light is red. The car is halted.");
+    }
 }
