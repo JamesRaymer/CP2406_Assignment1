224ab4c1b758477aa3ed6df7d858b7c2ea0b63f3
jamesraymer <james.raymer@my.jcu.edu.au>
2019-09-04 05:46:11 +1000

    Add constructor and helper methods for Vehicle class.

--- src/TrafficSimulator/Vehicle.java
+++ src/TrafficSimulator/Vehicle.java
@@ -1,8 +1,67 @@
 package TrafficSimulator;
 
 class Vehicle {
+    private String type;
+    private double length;
+    private double breadth;
 
+    Vehicle(String type) {
+        this.setType(type);
+        this.setLength(this.length);
+        this.setBreadth(this.breadth);
+    }
+
+    private void setType(String type) {
+        this.type = type;
+    }
+
+    String getType() {
+        return this.type;
+    }
+
+    private void setLength(double length) {
+        this.length = length;
+        length = 2.0;
+        String type = this.type;
+        byte count = -1;
+        switch(type) {
+            case "Bus":
+                count = 1;
+                break;
+            case "Car":
+                count = 0;
+                break;
+            case "Motorbike":
+                count = 2;
+        }
+
+        switch(count) {
+            case 0:
+                this.length = length;
+                break;
+            case 1:
+                this.length = length * 3.0;
+                break;
+            case 2:
+                this.length = length * 0.5;
+        }
+
+    }
+
+    double getLength() {
+        return this.length;
+    }
+
+    double getBreadth() {
+        this.calculateBreadth();
+        return this.breadth;
+    }
+
+    private void setBreadth(double breadth) {
+        this.breadth = breadth;
+    }
+
+    private void calculateBreadth() {
+        this.breadth = this.length / 2.0;
+    }
 }
-
-
-
