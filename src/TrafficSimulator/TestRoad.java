b5c52a4cfdbf2663d86111dd1861e2607f2a7172
jamesraymer <james.raymer@my.jcu.edu.au>
2019-09-04 05:37:54 +1000

    Add JUnit test for road class.

--- src/TrafficSimulator/TestRoad.java
+++ src/TrafficSimulator/TestRoad.java
@@ -1,6 +1,19 @@
 package TrafficSimulator;
-import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class TestRoad {
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TestRoad {
+    public TestRoad() {
+    }
+
+    @Test
+    public void checkStraightRoad() {
+        Road road = new Road(5, "Straight");
+        String type = "Straight";
+        int segments = 5;
+        road.setType("Straight");
+        Assertions.assertEquals(road.getSegments(), segments);
+        Assertions.assertEquals(road.getType(), type);
+    }
 }
