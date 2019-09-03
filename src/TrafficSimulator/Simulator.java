f792c843d01a7fe50437a5a8ed4a0c757a99f95b
jamesraymer <james.raymer@my.jcu.edu.au>
2019-09-04 05:52:36 +1000

    Add object instances and variables to Simulator Class.

--- src/TrafficSimulator/Simulator.java
+++ src/TrafficSimulator/Simulator.java
@@ -3,6 +3,12 @@ package TrafficSimulator;
 public class Simulator {
 
     public static void main(String[] args) {
-
+        Vehicle vehicle = new Vehicle("Bus");
+        Road road1 = new Road(6, "straight");
+        Road road2 = new Road(5, "Straight");
+        TrafficLight trafficLight = new TrafficLight(true);
+        int firstEndPoint = road1.getSegments();
+        int secondEndPoint = road2.getSegments();
+        byte roadSpeed = 50;
     }
 }
